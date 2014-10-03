ae1381d869b9ee1f2f7ff2df0521f20ff62c22ec
aolivares95 <antonio.olivares1995@gmail.com>
2014-10-02 20:06:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,14 +15,35 @@ var mainState = {
     create: function() { 
         // This function is called after the preload function     
         // Here we set up the game, display sprites, etc.
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
         this.ram=this.game.add.sprite(100,100,'ram');
+        
+        game.physics.arcade.enable(this.ram);
+        
+        this.ram.body.gravity.y = 1000;
+        
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        
+        spaceKey.onDown.add(this.jump, this);
+        
     },
+    
 
     update: function() {
         // This function is called 60 times per second    
         // It contains the game's logic
         
     },
+    jump: function() {
+            this.ram.body.velocity.y= -300;    
+        
+        }
+    }
+    
+    
+    
 };
 
 // Add and start the 'main' state to start the game
