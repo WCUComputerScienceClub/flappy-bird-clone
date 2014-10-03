f963e387f5ef66db5e9cd6a00deb5126bed59c9a
abiodunadeyemi <aa837647@wcupa.edu>
2014-10-02 20:10:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,17 +7,42 @@ var mainState = {
     preload: function() { 
         // This function will be executed at the beginning     
         // That's where we load the game's assets  
+        game.stage.backgroundColor = '#ed0077';
+        
+        game.load.image('ram','assets/ram.png');
     },
 
     create: function() { 
         // This function is called after the preload function     
         // Here we set up the game, display sprites, etc.  
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        //Draw the ram image on the screen
+        this.ram = this.game.add.sprite(100,100,'ram');
+        
+        // Tell Phaser we want the ram to have gravity
+        game.physics.arcade.enable(this.ram);
+        
+        // Add gravity to the ram so it falls
+        this.ram.body.gravity.y = 1000;
+        
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        
+        spaceKey.onDown.add(this.jump, this);
+        
+        
     },
 
     update: function() {
         // This function is called 60 times per second    
         // It contains the game's logic   
     },
+    
+    jump: function() {
+        this.ram.body.velocity.y = -300;
+        
+    }
 };
 
 // Add and start the 'main' state to start the game
