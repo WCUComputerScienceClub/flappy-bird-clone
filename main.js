27ce41be3e671a48c9268b110c9706489c74349b
Legilos <the_brucester@yahoo.com>
2014-10-02 20:17:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,41 @@ var mainState = {
 
     preload: function() { 
         // This function will be executed at the beginning     
-        // That's where we load the game's assets  
-        game.stage.backgroundColor = '#33ff33';
+        // That's where we load the game's assets 
+	game.stage.backgroundColor= '#0066ff';
+
+	game.load.image('ram', 'assets/ram.png');
     },
 
     create: function() { 
         // This function is called after the preload function     
         // Here we set up the game, display sprites, etc.  
+
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	this.ram = this.game.add.sprite(100,200,'ram');
+
+	game.physics.arcade.enable(this.ram);
+
+	this.ram.body.gravity.y = 1000;
+
+	var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+	
+
+	spaceKey.onDown.add(this.jump, this);
+
     },
 
     update: function() {
         // This function is called 60 times per second    
         // It contains the game's logic   
     },
+
+    jump: function(){
+	this.ram.body.velocity.y = -300;
+    }
+
 };
 
 // Add and start the 'main' state to start the game
