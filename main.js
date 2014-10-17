e7b670fe0c6621835657c98e6046292120585389
Joseph Cosentino <jcosentino11@gmail.com>
2014-10-16 20:23:22 -0400

    added pipes

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ var mainState = {
         // That's where we load the game's assets 
 
 		// Change the background Color
-		game.stage.backgroundColor = ‘#ff0000’;
+		game.stage.backgroundColor = "#ff0000";
 		
-		// Add an image
+		// Add ram and pipe images
 		game.load.image('ram','assets/ram.png');
+		game.load.image('pipe', 'assets/pipe.png');
 		
     },
 
@@ -30,11 +31,42 @@ var mainState = {
 		
 		// Add gravity to the ram so it falls
 		this.ram.body.gravity.y = 1000;
+
+
+		this.pipes = game.add.group(); //create a group
+		this.pipes.enableBody = true; // add physics
+		this.pipes.createMultiple(20, 'pipe'); //create 20 pipes
+
+
+		//add a timer
+		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 		
 		var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		
 		spaceKey.onDown.add(this.jump, this);
     },
+
+    addOnePipe: function(x,y){
+    	//Get the first dead pipe of the group
+    	var pipe = this.pipes.getFirstDead();
+    	//Set the position of the pipe
+    	pipe.reset(x,y);
+    	//add velocity to the pipe, making it move left
+    	pipe.body.velocity.x = -200;
+    	//remove the pipe when it's not visible
+    	pipe.checkWorldBounds = true;
+    	pipe.outOfBoundsKill = true;
+	},
+
+	addRowOfPipes: function(){
+		var hole = Math.floor(Math.random()*5) + 1;
+
+		//add the 6 pipes
+		for(var i=0;i<8;i++){
+			if(i != hole && i != hole + 1)
+				this.addOnePipe(400,i*60+10);
+		}
+	},
 
     update: function() {
         // This function is called 60 times per second    
